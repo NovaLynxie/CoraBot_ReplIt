6008e966e53d687461d57e75616ef3c1e79f14a0
Macky Fitz <pixelgamerx32@gmail.com>
2021-02-03 12:01:35 +0000

    Fixed woof.js command properly this time.

--- cora/commands/image/woof.js
+++ cora/commands/image/woof.js
@@ -15,18 +15,26 @@ module.exports = class WoofCommand extends Command {
     }
 
     async run(message) {
-        const { url } = await fetch('https://random.dog/woof.json')
-        console.log('url:'+url)
+      fetch('https://random.dog/woof.json')
+        .then(res => res.json())
+        .then(json => {
+          let url = json.url
+          generateEmbed(url)
+        })
+      const { url } = await fetch('https://random.dog/woof.json')
+      console.log('url:'+url)
+      function generateEmbed(url) {
         const embed = new MessageEmbed()
-            .setColor(0x00AE86)
-            .setTitle('Woof! 🐶')
-            .setDescription(stripIndents`
-                Das a cute doggo! ^w^
-                \`No image? There is a chance the image may not load.\`
-            `)
-            .setImage(url)
-            .setTimestamp();
+          .setColor(0x00AE86)
+          .setTitle('Woof! 🐶')
+          .setDescription(stripIndents`
+            Das a cute doggo! ^w^
+            \`No image? There is a chance the image may not load.\`
+          `)
+          .setImage(url)
+          .setTimestamp();
         return message.embed(embed);
+      }
     }
     // Command is broken despite embed working, image does not show.
     // Reason: Getting undefined from 'file' variable. 
