05fd9995e0021af8183f7efeb73ef931d201bcab
Macky Fitz <pixelgamerx32@gmail.com>
2021-04-04 08:55:17 +0100

    Update dbUpdater.js
    Replaced 'client' with runtime since bot client object is not available here.

--- cora/handlers/dbUpdater.js
+++ cora/handlers/dbUpdater.js
@@ -4,7 +4,7 @@ const SQLiteDB = require('better-sqlite3');
 const {database} = require('../handlers/bootLoader');
 const {storage, settings} = database;
 
-let db;
+let db, runtime;
 if (storage === "replit") {
   db = new ReplitDB();
 } else
@@ -20,19 +20,18 @@ function initSqliteDB() {
     db.prepare("CREATE TABLE webdata (id TEXT PRIMARY KEY, uptime INTEGER, guilds INTEGER, members INTEGER, allch INTEGER, txtch INTEGER, vch INTEGER);").run();
   }
   // Generate prepare statements to get and set relevant data where needed.
-  client.getUptime = db.prepare("SELECT * FROM webdata WHERE uptime = ?")
-  client.setUptime = db.prepare("INSERT OR REPLACE INTO webdata (uptime) VALUES (@uptime);");
-  client.getMembers = db.prepare("SELECT * FROM webdata WHERE members = ?")
-  client.setMembers = db.prepare("INSERT OR REPLACE INTO webdata (members) VALUES (@members);");
-  client.getGuilds = db.prepare("SELECT * FROM webdata WHERE guilds = ?")
-  client.setGuilds = db.prepare("INSERT OR REPLACE INTO webdata (guilds) VALUES (@guilds);");
-  client.getAllCh = db.prepare("SELECT * FROM webdata WHERE allch = ?")
-  client.setAllCh = db.prepare("INSERT OR REPLACE INTO webdata (allch) VALUES (@allch);");
-  client.getTxtCh = db.prepare("SELECT * FROM webdata WHERE txtch = ?")
-  client.setTxtCh = db.prepare("INSERT OR REPLACE INTO webdata (txtch) VALUES (@txtch);");
-  client.getVCh = db.prepare("SELECT * FROM webdata WHERE vch = ?")
-  client.setVCh = db.prepare("INSERT OR REPLACE INTO webdata (vch) VALUES (@vch);");
-
+  runtime.getUptime = db.prepare("SELECT * FROM webdata WHERE uptime = ?")
+  runtime.setUptime = db.prepare("INSERT OR REPLACE INTO webdata (uptime) VALUES (@uptime);");
+  runtime.getMembers = db.prepare("SELECT * FROM webdata WHERE members = ?")
+  runtime.setMembers = db.prepare("INSERT OR REPLACE INTO webdata (members) VALUES (@members);");
+  runtime.getGuilds = db.prepare("SELECT * FROM webdata WHERE guilds = ?")
+  runtime.setGuilds = db.prepare("INSERT OR REPLACE INTO webdata (guilds) VALUES (@guilds);");
+  runtime.getAllCh = db.prepare("SELECT * FROM webdata WHERE allch = ?")
+  runtime.setAllCh = db.prepare("INSERT OR REPLACE INTO webdata (allch) VALUES (@allch);");
+  runtime.getTxtCh = db.prepare("SELECT * FROM webdata WHERE txtch = ?")
+  runtime.setTxtCh = db.prepare("INSERT OR REPLACE INTO webdata (txtch) VALUES (@txtch);");
+  runtime.getVCh = db.prepare("SELECT * FROM webdata WHERE vch = ?")
+  runtime.setVCh = db.prepare("INSERT OR REPLACE INTO webdata (vch) VALUES (@vch);");
 }
 
 function sqlitedbUpdate(uptime, guilds, members, allch, txtch, vch) {
