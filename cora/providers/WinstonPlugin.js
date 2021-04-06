f046c0874e91a7fb0bd56344673ddd1b201b7084
Macky Fitz <>
2021-04-06 08:46:11 +0000

    Bump to v2.7.3 Re-added Winston DailyRotateFile. Enabled relevant code in WinstonPlugin.js

--- cora/providers/WinstonPlugin.js
+++ cora/providers/WinstonPlugin.js
@@ -1,6 +1,6 @@
 require('dotenv').config() // load .env as early as possible
 const winston = require('winston');
-//require('winston-daily-rotate-file'); // does not work, disabled.
+require('winston-daily-rotate-file'); //re-added for daily logs.
 const {addColors, createLogger, format, transports} = winston
 const {combine, colorize, errors, json, timestamp, printf} = format;
 var {logLevel} = process.env; // gets logLevel from os process.env vars
@@ -51,6 +51,23 @@ const logger = createLogger({
         printf(info => `(${info.timestamp}) [${info.level}] ${info.message}`),
       ),
       handleExceptions: true
+    }),
+    new transports.DailyRotateFile({
+      
+      filename: 'corabot-%DATE%.log',
+      datePattern: 'DD-MM-YY',
+      zippedArchive: true,
+      maxSize: '50m',
+      maxFiles: '14d',
+      level: 'debug'
+    })
+    new transports.DailyRotateFile({
+      filename: 'debug-%DATE%.log',
+      datePattern: 'DD-MM-YY',
+      zippedArchive: true,
+      maxSize: '50m',
+      maxFiles: '14d',
+      level: 'debug'
     })
   ],
 });
